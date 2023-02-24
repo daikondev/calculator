a143f6c35a9d306cc478bac83a27cf4e29825515
whiteraddish <whiteraddishdev@gmail.com>
2023-02-24 03:06:12 +0200

    Bound buttons to array, gave them event listeners, basic display function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const buttons = document.querySelectorAll(".number");
+const buttonArray = Array.from(buttons);
+for (let i = 0; i < buttonArray.length; i++){
+    buttonArray[i].addEventListener('click', () => {
+        console.log(`${buttonArray[i].innerText}`);
+        changeDisplay(`${buttonArray[i].innerText}`);
+    })
+}
+
+const display = document.querySelector(".display");
+
+
 function add(a) {
     return (b) => {
         return a + b;
@@ -22,11 +34,14 @@ function divide(a){
         }
     };
 }
-
 function operate(a){
     return (b) => {
         return (func) =>{
             return func(a)(b);
         };
     };
+}
+//TODO: change displaying functionality so that it can do multiple digits.
+function changeDisplay (num) {
+    display.innerText = num;
 }
